7c60a0353d02fba85baf04f826413b8069a4f845
Rayzanghua <2424896931@qq.com>
2018-05-10 10:21:26 +0800

    更新第三章

---
+++ shiro-example/shiro-example-chapter3/src/main/java/com/github/zhangkaitao/shiro/chapter3/RoleTest.java
@@ -0,0 +1,23 @@
+package com.github.zhangkaitao.shiro.chapter3;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RoleTest extends BaseTest{
+
+	@Test  
+	public void testHasRole() {  
+	    login("classpath:shiro-role.ini", "zhang", "123");  
+	    //判断拥有角色：role1  
+	    Assert.assertTrue(subject().hasRole("role1"));  
+	    //判断拥有角色：role1 and role2  
+	    Assert.assertTrue(subject().hasAllRoles(Arrays.asList("role1", "role2")));  
+	    //判断拥有角色：role1 and role2 and !role3  
+	    boolean[] result = subject().hasRoles(Arrays.asList("role1", "role2", "role3"));  
+	    Assert.assertEquals(true, result[0]);  
+	    Assert.assertEquals(true, result[1]);  
+	    Assert.assertEquals(false, result[2]);  
+	}   
+}
